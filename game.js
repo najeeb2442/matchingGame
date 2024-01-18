4e8d204f2359063af6afe396d1027fd1ced13e65
najeeb2442 <s.najib2442@gmail.com>
2024-01-18 15:40:09 +0300

    the basic functionality works fine

--- game.js
+++ game.js
@@ -1,39 +1,77 @@
 const main = document.querySelector("main")
-const cards = document.querySelectorAll(".card")
+//const cards = document.querySelectorAll(".card")
 let cardsFliped = 0
 let firstCard = undefined
+const colors = []
+
+const randomColor = () => {
+  return "#" + parseInt(Math.random() * 1000000)
+}
+const randomNumber = (num) => {
+  return parseInt(Math.random() * 1000000) % num
+}
 
 const makeCards = (rows, columns) => {
+  // not finished
   main.innerHTML = ""
+  let cells = rows * columns
+
+  for (let index = 0; index < cells / 2; index++) {
+    const color = randomColor()
+    colors.push(color)
+    colors.push(color)
+  }
+  for (let index = 0; index < cells; ) {
+    const button = document.createElement("button")
+    let num = randomNumber(cells)
+    button.classList.add("card")
+
+    button.innerHTML = `<div value="${colors[num]}" class="front"></div>  `
+
+    //button.style.backgroundColor = colors[num]
+    colors.splice(num, 1)
+    cells--
+    main.append(button)
+  }
 
   // random stuff
-  const results = response.data.results
-  list.innerHTML = ""
-  results.forEach((movie) => {
-    const item = document.createElement("div")
-    item.style.backgroundImage = `url(${IMAGE_BASE_PATH + movie.backdrop_path})`
-    item.classList.add("item")
+  //const results = response.data.results
+  // main.innerHTML = ""
+  // results.forEach((movie) => {
+  //   const item = document.createElement("div")
+  //   item.style.backgroundImage = `url(${IMAGE_BASE_PATH + movie.backdrop_path})`
+  //   item.classList.add("item")
 
-    //backdrop_path
-    item.innerHTML = `<img src=${IMAGE_BASE_PATH + movie.poster_path}>
-    <h1>${movie.original_title} (${movie.release_date.substring(0, 4)})</h1>
-    <p>${movie.overview}</p>
-    `
-    list.append(item)
-  })
+  //   //backdrop_path
+  //   item.innerHTML = `<img src=${IMAGE_BASE_PATH + movie.poster_path}>
+  //   <h1>${movie.original_title} (${movie.release_date.substring(0, 4)})</h1>
+  //   <p>${movie.overview}</p>
+  //   `
+  //   main.append(item)
+  // })
 }
 
 const flip = (card) => {
   if (card.children[0].className == "front") {
     card.children[0].className = "back"
+    card.children[0].style.backgroundColor =
+      card.children[0].getAttribute("value")
+    console.log(card.children[0])
   } else {
     card.children[0].className = "front"
+    card.children[0].style.backgroundColor = ""
   }
 }
 
+////////////// make cards
+makeCards(4, 6)
+
+// make event listener to each card
+let cards = document.querySelectorAll(".card")
 cards.forEach((card) => {
   card.addEventListener("click", () => {
     if (cardsFliped == 0) {
+      console.log(cardsFliped)
       flip(card)
       card.setAttribute("disabled", true)
       cardsFliped += 1
@@ -50,13 +88,19 @@ cards.forEach((card) => {
         firstCard.children[0].style.backgroundColor !=
         card.children[0].style.backgroundColor
       ) {
-        flip(card)
-        flip(firstCard)
-        card.setAttribute("disabled", false)
-        card.setAttribute("disabled", false)
+        setTimeout(() => {
+          flip(card)
+          flip(firstCard)
+          card.removeAttribute("disabled")
+          firstCard.removeAttribute("disabled")
+        }, 500)
+
+        // card.toggleAttribute("disabled")
       } else {
+        //console.log()
         card.setAttribute("disabled", true)
         firstCard.setAttribute("disabled", true)
+        console.log(card)
       }
     }
 
