bc1b232c9f9271f88a131f04b9f03efc9ead4dc8
najeeb2442 <s.najib2442@gmail.com>
2024-01-21 16:00:39 +0300

    finishing touchs

--- game.js
+++ game.js
@@ -5,16 +5,12 @@ const numColumns = document.querySelector(".columns")
 const message = document.querySelector(".message")
 
 const wins = document.querySelector(".wins")
-//const loses = document.querySelector(".loses")
 const moves = document.querySelector(".moves")
 const mistakes = document.querySelector(".mistakes")
-//const cards = document.querySelectorAll(".card")
 let cWins = 0
-//let cLoses = 0
 let cMoves = 0
 let cMistakes = 0
 let numOfCells = 4
-
 let cardsFliped = 0
 let firstCard = undefined
 const colors = []
@@ -58,8 +54,6 @@ makeEventListener = () => {
             card.removeAttribute("disabled")
             firstCard.removeAttribute("disabled")
           }, 500)
-
-          // card.toggleAttribute("disabled")
         } else {
           cMoves += 1
           moves.innerText = `moves: ${cMoves}`
@@ -89,10 +83,8 @@ const makeCards = (rows, columns) => {
   //sizing
   hSize = "calc(100% /" + rows + ")"
   wSize = "calc(100% /" + columns + ")"
-
   hS = "calc(100vh /" + rows + ")"
   wS = "calc(100vw /" + columns + ")"
-
   for (let index = 0; index < cells / 2; index++) {
     const color = randomColor()
     colors.push(color)
@@ -102,17 +94,12 @@ const makeCards = (rows, columns) => {
     const button = document.createElement("button")
     let num = randomNumber(cells)
     button.classList.add("card")
-
     button.innerHTML = `<div value="${colors[num]}" class="front"></div>  `
-
-    //button.style.backgroundColor = colors[num]
     colors.splice(num, 1)
     cells--
-
     // adjusting the size of the cards
     button.setAttribute("style", "width: " + wSize + "; height: " + hSize)
     button.children[0].setAttribute("style", "width: " + wS + "; height: " + hS)
-
     main.append(button)
   }
   makeEventListener()
